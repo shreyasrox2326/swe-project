da03e3be2b90bd24252c0c98c5778908daa34b58
shreyasrox2326 <110967852+shreyasrox2326@users.noreply.github.com>
2026-04-14 16:38:05 +0530

    mail errors

--- demo/src/main/java/com/example/demo/EmailDeliveryService.java
+++ demo/src/main/java/com/example/demo/EmailDeliveryService.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class EmailDeliveryService {
@@ -20,11 +23,19 @@ public class EmailDeliveryService {
     }
 
     public void sendPlainText(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(fromAddress);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(fromAddress);
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            mailSender.send(message);
+        } catch (MailException exception) {
+            throw new ResponseStatusException(
+                    HttpStatus.SERVICE_UNAVAILABLE,
+                    "Unable to send email right now. Check SMTP configuration and try again.",
+                    exception
+            );
+        }
     }
 }
